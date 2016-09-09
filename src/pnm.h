37f88f032df6d0d8f5a186b4ad503a0ca25dd3b5
Christopher Robert Philabaum <christopher@philabaum.me>
2016-09-08 22:52:09 -0700

    Increased max channel size to 4 for alpha.

--- src/pnm.h
+++ src/pnm.h
@@ -18,7 +18,7 @@ typedef struct pnmHeader {
 } pnmHeader;
 
 typedef struct pixel {
-    unsigned char channels[3];
+    unsigned char channels[4];
 } pixel;
 
 int readHeader(pnmHeader& header, FILE* inputFd);
