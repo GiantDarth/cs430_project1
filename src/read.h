4ed1a48f82e0d658a0062fac643d8d4505db5f28
Christopher Robert Philabaum <christopher@philabaum.me>
2016-09-11 17:31:51 -0700

    Fixed leftover '&' reference operator

--- src/read.h
+++ src/read.h
@@ -5,7 +5,7 @@
 
 #include "pnm.h"
 
-int readHeader(pnmHeader& header, FILE* inputFd);
+int readHeader(pnmHeader* header, FILE* inputFd);
 int readBody(pnmHeader header, pixel* pixels, FILE* inputFd);
 
 #endif // CS430_PNM_READ_H
